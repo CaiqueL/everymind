43ff63debc30bf1d4dd53f0621b8193a49f6db64
CaiqueL <lpscaca@gmail.com>
2023-09-21 14:16:16 -0300

    fix pra aparecer os componentes na direita da tela

--- src/routes/TelasCandidato/Candidato/Candidato.jsx
+++ src/routes/TelasCandidato/Candidato/Candidato.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 const Candidato = () => {
   const containerStyle = {
     position: "fixed",
@@ -23,50 +24,59 @@ const Candidato = () => {
   };
 
   return (
-    <div style={containerStyle}>
-      <div>
-        {/* <img
-          src="path_to_image1"
-          alt="Image 1"
-          style={{ width: "100%", marginBottom: "10px" }}
-        />
-        <img
-          src="path_to_image2"
-          alt="Image 2"
-          style={{ width: "100%", marginBottom: "10px" }}
-        /> */}
+    <>
+      <div style={containerStyle}>
+        
         <div>
-          <img
-            src="https://c.animaapp.com/RLL3gMW3/img/image-11-1@2x.png"
-            style={{
-              width: "100%",
-              marginBottom: "20px",
-              height: "100px",
-              border: "none",
-            }}
+          {/* <img
+            src="path_to_image1"
+            alt="Image 1"
+            style={{ width: "100%", marginBottom: "10px" }}
           />
+          <img
+            src="path_to_image2"
+            alt="Image 2"
+            style={{ width: "100%", marginBottom: "10px" }}
+          /> */}
+          <div>
+            <img
+              src="https://c.animaapp.com/RLL3gMW3/img/image-11-1@2x.png"
+              style={{
+                width: "100%",
+                marginBottom: "20px",
+                height: "100px",
+                border: "none",
+              }}
+            />
+          </div>
+          <button
+            style={{
+              flex: 1,
+              marginRight: "5px",
+              backgroundColor: " rgba(0, 255, 149, 0.148)",
+              color: "#000000",
+              outline: "none",
+              display: "flex",
+              width: "100%",
+            }}
+          >
+            <NavLink to="/Candidato/Perfil">Perfil</NavLink>
+          </button>
+          <button style={buttonStyle}>Minhas Vagas</button>
+          <button style={buttonStyle}>Vagas Disponíveis</button>
+          <button style={buttonStyle}>Testes</button>
+          <button style={buttonStyle}>Entrevistas</button>
+          <button style={buttonStyle}>Feedbacks</button>
         </div>
-        <button
-          style={{
-            flex: 1,
-            marginRight: "5px",
-            backgroundColor: " rgba(0, 255, 149, 0.148)",
-            color: "#000000",
-            outline: "none",
-            display: "flex",
-            width: "100%",
-          }}
-        >
-          <NavLink to="/Candidato/Perfil">Perfil</NavLink>
-        </button>
-        <button style={buttonStyle}>Minhas Vagas</button>
-        <button style={buttonStyle}>Vagas Disponíveis</button>
-        <button style={buttonStyle}>Testes</button>
-        <button style={buttonStyle}>Entrevistas</button>
-        <button style={buttonStyle}>Feedbacks</button>
-      </div>
+    </div>
+    <div>
+
       <Outlet/>
-   </div>
+
+    </div>
+    
+
+   </>
   );
 };
 
--- src/routes/TelasCandidato/Perfil/Perfil.jsx
+++ src/routes/TelasCandidato/Perfil/Perfil.jsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom";
 import { BigCard } from "../../../components/BigCard";
+
+function ComponenteAleatorio(){
+  return(
+    <h1 style={{backgroundcolor:"black"}}>componente aleatorio teste</h1>
+  )
+}
+
 const Perfil = () => {
     return (
-    
-        <h1>teste</h1>
-        
-        
-        
+      <div>
+        <BigCard/>
+      </div>  
     )
 };
 
